17018a957c41f3fd948e2188b2b9da2d92a22a0a
RowenStipe <rowen.stipe@gmail.com>
2016-02-05 23:23:22 -0500

    Added Copyright info and distinguish between action messages and "speech"

--- src/nms/notificationmanager.cpp
+++ src/nms/notificationmanager.cpp
@@ -1,3 +1,22 @@
+/*
+    Copyright © 2016 by The qTox Project
+
+    This file is part of qTox, a Qt-based graphical interface for Tox.
+
+    qTox is libre software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    qTox is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with qTox.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "notificationmanager.h"
 #include "src/friend.h"
 #include "src/nexus.h"
@@ -10,7 +29,13 @@
 #include <QDebug>
 #include <QApplication>
 
-
+QString msgType(int msgType)
+{
+    if(msgType == 1)
+        return "Action";
+    if(msgType == 0)
+        return "Speech";
+}
 
 void NotificationManager::hookFriendMessage(uint32_t friendId, int type, const uint8_t* cMessage, int cMessageSize)
 {
@@ -19,7 +44,7 @@ void NotificationManager::hookFriendMessage(uint32_t friendId, int type, const u
     if (QApplication::focusWidget() <= 0)
     {
         qDebug() << "NMS: Friend ID: "+Username;
-        qDebug() << "NMS: Message Type: "+QString(type);
+        qDebug() << "NMS: Message Type: "+msgType(type);
         qDebug() << "NMS: Message: "+CString::toString(cMessage,cMessageSize);
     }
 }
